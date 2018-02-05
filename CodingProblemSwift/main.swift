16eaa29d43c1c27a74a5bced05531d9962904812
iqbalansyori <iqbal@icehousecorp.com>
2018-02-05 17:10:10 +0700

    Initial Commit

---
+++ CodingProblemSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodingProblemSwift
+//
+//  Created by Iqbal Ansyori on 2/5/18.
+//  Copyright © 2018 Iqbal Ansyori. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
